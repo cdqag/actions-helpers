b2bce1e4c03383165125091688eb46860c8201dc
Tomasz Skalniak <tomasz.skalniak@cdq.com>
2025-02-21 16:33:15 +0100

    feat: Add yaml_helpers.sh

--- test/test_helper/common.bash
+++ test/test_helper/common.bash
@@ -1,12 +1,20 @@
 DIR="$(cd "$(dirname "$BATS_TEST_FILENAME")/.." >/dev/null 2>&1 && pwd)"
 PATH="$DIR/../src:$PATH"
 
+workdir=/tmp/not-yet-set
+
 ORIGINAL_RUNNER_DEBUG="$RUNNER_DEBUG"
 ORIGINAL_GITHUB_ACTIONS="$GITHUB_ACTIONS"
 ORIGINAL_GITHUB_ENV="$GITHUB_ENV"
 ORIGINAL_GITHUB_OUTPUT="$GITHUB_OUTPUT"
 
 function common_setup() {
+	# Prepare a work directory
+	workdir=$(mktemp -d)
+
+	# Copy fixtures
+	cp -r test/fixtures/. "$workdir"
+
 	RUNNER_DEBUG=""
 	GITHUB_ACTIONS=""
 	GITHUB_ENV=$(mktemp)
@@ -14,6 +22,8 @@ function common_setup() {
 }
 
 function common_teardown() {
+	rm -rf "$workdir"
+
 	rm -f "$GITHUB_ENV"
 	rm -f "$GITHUB_OUTPUT"
 
